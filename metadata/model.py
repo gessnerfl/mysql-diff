16209e5691201fc8c56552ebc823f25ff3092244
Florian Gessner <florian.gessner@tis.biz>
2021-08-31 15:51:11 +0200

    add super class/interface for all metadata objects

--- metadata/model.py
+++ metadata/model.py
@@ -2,10 +2,19 @@ from typing import List
 import json
 
 
-class RoutineParameter:
+class MetaData:
+    def __init__(self):
+        pass
+
+    def to_dict(self):
+        raise Exception("Not implemented yet")
+
+
+class RoutineParameter(MetaData):
     def __init__(self, parameter_name: str, ordinal_position: str, parameter_mode: str, data_type: str,
                  character_maximum_length: str, character_octet_length: str, numeric_precision: str, numeric_scale: str,
                  datetime_precision: str, character_set_name: str, collation_name: str, dtd_identifier: str):
+        MetaData.__init__(self)
         self.parameter_name = parameter_name
         self.ordinal_position = ordinal_position
         self.parameter_mode = parameter_mode
@@ -23,7 +32,7 @@ class RoutineParameter:
         return self.__dict__
 
 
-class RoutineMetaData:
+class RoutineMetaData(MetaData):
     def __init__(self, schema: str, name: str, routine_type: str, data_type: str, character_max_length: str,
                  character_octet_length: str, numeric_precision: str, numeric_scale: str, datetime_precision: str,
                  character_set_name: str, collation_name: str, dtd_identifier: str, routine_body: str,
@@ -31,6 +40,7 @@ class RoutineMetaData:
                  is_deterministic: str, sql_data_access: str, sql_path: str, security_type: str, sql_mode: str,
                  routine_comment: str, definer: str, character_set_client: str, collation_connection: str,
                  database_collation: str):
+        MetaData.__init__(self)
         self.schema = schema
         self.name = name
         self.routine_type = routine_type
@@ -64,9 +74,10 @@ class RoutineMetaData:
         return self.__dict__
 
 
-class ViewMetaData:
+class ViewMetaData(MetaData):
     def __init__(self, schema: str, name: str, definition: str, check_option: str, is_updatable: str, definer: str,
                  security_type: str, character_set_client: str, collation_connection: str):
+        MetaData.__init__(self)
         self.schema = schema
         self.name = name
         self.definition = definition
@@ -81,9 +92,10 @@ class ViewMetaData:
         return self.__dict__
 
 
-class KeyColumnUsage:
+class KeyColumnUsage(MetaData):
     def __init__(self, column_name: str, ordinal_position: str, position_in_unique_constraint: str,
                  referenced_table_schema: str, referenced_table_name: str, referenced_column_name: str):
+        MetaData.__init__(self)
         self.column_name = column_name
         self.ordinal_position = ordinal_position
         self.position_in_unique_constraint = position_in_unique_constraint
@@ -95,9 +107,10 @@ class KeyColumnUsage:
         return self.__dict__
 
 
-class ReferentialConstraint:
+class ReferentialConstraint(MetaData):
     def __init__(self, constraint_name: str, unique_constraint_name: str, match_option: str,
                  update_rule: str, delete_rule: str, referenced_table_name: str):
+        MetaData.__init__(self)
         self.constraint_name = constraint_name
         self.unique_constraint_name = unique_constraint_name
         self.match_option = match_option
@@ -109,10 +122,11 @@ class ReferentialConstraint:
         return self.__dict__
 
 
-class Index:
+class Index(MetaData):
     def __init__(self, index_name: str, non_unique: str, seq_in_index: str, column_name: str, collation: str,
                  cardinality: str, sub_part: str, packed: str, nullable: str, index_type: str, comment: str,
                  index_comment: str):
+        MetaData.__init__(self)
         self.index_name = index_name
         self.non_unique = non_unique
         self.seq_in_index = seq_in_index
@@ -130,11 +144,12 @@ class Index:
         return self.__dict__
 
 
-class ColumnMetaData:
+class ColumnMetaData(MetaData):
     def __init__(self, column_name: str, ordinal_position: str, column_default: str, is_nullable: str, data_type: str,
                  character_maximum_length: str, character_octet_length: str, numeric_precision: str, numeric_scale: str,
                  datetime_precision: str, character_set_name: str, collation_name: str, column_type: str,
                  column_key: str, extra: str, privileges: str, column_comment: str, generation_expression: str):
+        MetaData.__init__(self)
         self.column_name = column_name
         self.ordinal_position = ordinal_position
         self.column_default = column_default
@@ -158,9 +173,10 @@ class ColumnMetaData:
         return self.__dict__
 
 
-class TableMetaData:
+class TableMetaData(MetaData):
     def __init__(self, schema: str, name: str, engine: str, version: str, row_format: str, auto_increment: str,
                  table_collation: str, create_option: str, table_comment: str):
+        MetaData.__init__(self)
         self.schema = schema
         self.name = name
         self.engine = engine
@@ -179,8 +195,9 @@ class TableMetaData:
         return self.__dict__
 
 
-class SchemaMetaData:
+class SchemaMetaData(MetaData):
     def __init__(self, name: str, default_character_set: str, default_collation: str):
+        MetaData.__init__(self)
         self.name = name
         self.default_character_set = default_character_set
         self.default_collation = default_collation
@@ -193,9 +210,10 @@ def to_json(o):
     return o.to_dict()
 
 
-class MetaDataCatalog:
+class MetaDataCatalog(MetaData):
     def __init__(self, schemas: List[SchemaMetaData], tables: List[TableMetaData], views: List[ViewMetaData],
                  routines: List[RoutineMetaData]):
+        MetaData.__init__(self)
         self.schemas = schemas
         self.tables = tables
         self.views = views
